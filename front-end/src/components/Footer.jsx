26e657566b588912dd0936b02c9d32b7180a3c61
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-06-23 12:58:21 -0600

    added header component
    added site navigation, a responsive header and started working on the footer

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 
 export default function Footer() {
     
 
     return (
-        <>
-            Footer
-        </>
+        <footer className='footer'>
+            <div className="footer__background">
+                <svg className='bezier__curve' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 240"><path fill="#B1C63D" fillOpacity="1" d="M0,160L48,160C96,160,192,160,288,170.7C384,181,480,203,576,202.7C672,203,768,181,864,154.7C960,128,1056,96,1152,85.3C1248,75,1344,85,1392,90.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+                <div className="hero_rectangle">
+                </div>
+            </div>
+            <div className="footer__content container">
+                <div className="footer__logoContainer">
+                        <Link to='/' className='footer__link'>
+                            <StaticImage
+                                className='footer__logo'
+                                src="../../images/fttd-logo-dk.png"
+                                alt="logo image for for the love of dog"
+                                
+                            />
+                        </Link>
+                </div>
+            </div>
+            
+        </footer>
     )
 }
--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
+//dependencies
+
+import React, {useState} from 'react'
+import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
+
+// icons
+
+import { MdSearch } from 'react-icons/md'
+import { RiMenu5Line } from 'react-icons/ri'
+import { CgClose } from 'react-icons/cg'
+
+//components
+
+import { navRouter } from '../constants/navrouter'
 
 export default function Header() {
     
+    const [ isNavOpen, setIsNavOpen ] = useState(false)
+
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen)
+    }
+
+    const handleNavClick = () => {
+        if(isNavOpen){
+            setIsNavOpen(false)
+        }
+    }
+
+    const handleSearchModal = () => {
+       
+    }
 
     return (
-        <>
-            Header
-        </>
+        <header className='header'>
+            <div className="container">
+                <div className="header__logoContainer">
+                    <Link to='/' className='header__link'>
+                        <StaticImage
+                            className='header__logo'
+                            src="../../images/fttd-logo-dk.png"
+                            alt="logo image for for the love of dog"
+                            
+                        />
+                    </Link>
+                </div>
+                <button className='mobileNav__toggle' id="mobileNav__toggle" aria-controls='site__nav' aria-expanded="false" onClick={toggleNav}>
+                    <span className='sr-only'>Menu</span>
+                    {isNavOpen ? ( <CgClose/> ) : ( <RiMenu5Line/> )}
+                </button>
+                <nav className={isNavOpen ? 'navbar shownav' : 'navbar'} id="navbar">
+                    <ul className="navmenu" id="sitenav">
+                        <li>
+                            <div 
+                                className="searchIcon__wrapper" 
+                                onClick={handleSearchModal} 
+                                onKeyDown={handleSearchModal}
+                                role="button"
+                                tabIndex={0}
+                            >
+                                <MdSearch className='searchIcon'></MdSearch>
+                            </div>
+                        </li>
+                        {navRouter.map(navItem => (
+                            <li key={navItem.path} className="nav__item">
+                                <Link className='menu__link' to={navItem.path} onClick={handleNavClick}><span aria-hidden="true" className='navID'>{navItem.id}: </span>{navItem.title}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+        </header>
     )
 }
