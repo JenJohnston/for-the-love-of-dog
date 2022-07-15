d580e63fcf9ff8b8a0347bcd0ca1af4acac1c026
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-07-14 20:57:09 -0600

    Update gatsby-browser.jsx

--- front-end/gatsby-browser.jsx
+++ front-end/gatsby-browser.jsx
@@ -4,9 +4,12 @@ import 'aos/dist/aos.css';
 import React from "react";
 import Layout from "./src/components/Layout";
 
-AOS.init()
+
 
 export const wrapPageElement = ({ element, props }) => (
  
-  <Layout {...props}>{element}</Layout>
+  <Layout {...props}>
+    {AOS.init()}
+    {element}
+  </Layout>
 );
