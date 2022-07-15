ac276ae2b257f5f7f4cf3e773db410c30f68f177
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-07-14 21:11:11 -0600

    Update gatsby-ssr.js

--- front-end/gatsby-ssr.js
+++ front-end/gatsby-ssr.js
@@ -1,14 +1,17 @@
 import "./src/scss/index.scss";
 import AOS, { init } from 'aos';
 import 'aos/dist/aos.css'; 
-import React from "react";
+import React, {useEffect} from "react";
 import Layout from "./src/components/Layout";
+
+useEffect(() => {
+  AOS.init()
+})
 
 export const wrapPageElement = ({ element, props }) => (
 
- 
+  
   <Layout {...props}>
-    {AOS.init()}
     {element}
   </Layout>
 );
