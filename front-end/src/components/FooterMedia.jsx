6a949204347fd68d4bbf4662dbda87f8f0a8c08a
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-07-17 15:11:56 -0600

    Update FooterMedia.jsx

--- front-end/src/components/FooterMedia.jsx
+++ front-end/src/components/FooterMedia.jsx
@@ -12,7 +12,7 @@ export default function FooterMedia() {
 
     return (
         <div className='media__links'>
-            <a to="https://www.facebook.com/fortheloveofdogCA">
+            <a href="https://www.facebook.com/fortheloveofdogCA">
                 <FaFacebook className='media__link'/>
             </a>
             <Link to="/">
